43d58562cd5445a9c365cb578677dd57d21b9c4f
MedCy1 <MedDev1337@protonmail.com>
2022-01-19 16:34:21 +0100

    Update v1.0.1

--- NitroGen.py
+++ NitroGen.py
@@ -4,8 +4,13 @@ import string
 import requests
 from colorama import Fore, Back, Style
 import os
+import ctypes
 
+kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+
+os.system('mode con: cols=126 lines=30')
 os.system("cls")
+kernel32.SetConsoleTitleW(u"The V Nitro Gen")
 
 version = ("v1.0.0")
 
@@ -33,9 +38,11 @@ while b < a:
     f.write('https://discord.gift/' + ''.join(random.choice(letters) for i in range(16)) + '\n')
     print('\u001b[32mhttps://discord.gift/' + ''.join(random.choice(letters) for i in range(16)) + '\u001b[37m')
     b += 1
+    kernel32.SetConsoleTitleW(f"Nitro generated: {b} | Valid: {Valid} | Invalid {Invalid}")
 
 os.system("cls")
 
+kernel32.SetConsoleTitleW(u"The V Nitro Check")
 print(f"""\u001b[31m 
 ▄▄▄█████▓ ██░ ██ ▓█████     ██▒   █▓    ███▄    █  ██▓▄▄▄█████▓ ██▀███   ▒█████      ▄████▄   ██░ ██ ▓█████  ▄████▄   ██ ▄█▀
 ▓  ██▒ ▓▒▓██░ ██▒▓█   ▀    ▓██░   █▒    ██ ▀█   █ ▓██▒▓  ██▒ ▓▒▓██ ▒ ██▒▒██▒  ██▒   ▒██▀ ▀█  ▓██░ ██▒▓█   ▀ ▒██▀ ▀█   ██▄█▒ 
@@ -63,11 +70,14 @@ if Check in ['y', 'Y', 'yes', 'Yes', 'YES']:
                 print(f"\u001b[37m\n\n Valid Code\u001b[37m\n | \u001b[37m{nitro}\u001b[37m")
                 g.write(f'{nitro}\n')
                 Valid += 1
+                kernel32.SetConsoleTitleW(f"Nitro generated: {b} | Valid: {Valid} | Invalid {Invalid}")
             else:
                 print(f"\u001b[31mInvalid Code\u001b[37m | \u001b[31m{nitro}\u001b[37m\n")
                 f.write(f'{nitro}\n')
                 Invalid += 1
+                kernel32.SetConsoleTitleW(f"Nitro generated: {b} | Valid: {Valid} | Invalid {Invalid}")
     print(f"You have \u001b[32m{Valid} Valid\u001b[37m and \u001b[31m{Invalid} Invalid\u001b[37m code")
+    input()
 else:
     print('Your Codes are in the Nitro.txt file')
     input()
