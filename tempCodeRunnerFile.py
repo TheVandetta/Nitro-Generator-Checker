080cd8fa7b3e61a39106ad62a85b911532830e0d
MedCy1 <MedDev1337@protonmail.com>
2022-01-19 14:52:52 +0100

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-while b < a:
-    letters = string.ascii_letters + string.digits
-    f = open("Nitro.txt", "a")
-    f.write('https://discord.gift/' + ''.join(random.choice(letters) for i in range(16)) + '\n')
-    print('\u001b[32mhttps://discord.gift/' + ''.join(random.choice(letters) for i in range(16)) + '\u001b[37m')
-    b += 1
